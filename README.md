03a784238e0e7174d3012b2b9e6b5259e10c65be
vijaykanaujia <vijaykanaujia3@gmail.com>
2022-08-16 20:38:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-8huzus
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-8huzus)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+import './tutorial/w3school/simple-type.ts';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter1</h1>`;
